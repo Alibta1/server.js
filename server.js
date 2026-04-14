31f57d5451740a49a7b956e96ebe454886cb0687
Alibta1 <161652565+Alibta1@users.noreply.github.com>
2026-04-14 13:10:40 +0000

    Change event_name to Purchase

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 const http = require('http');
 const axios = require('axios');
-const crypto = require('crypto');
 
 const PORT = process.env.PORT || 3000;
 const PIXEL_ID = '359988621109574';
-const ACCESS_TOKEN = 'EABVsPwmVH20BREL54FbOJyk1pfjZBdZBv3OQSv7cXpFXJEIyxXr9QaZBRNZCQZA3BZCExH8pQga58AryB2MLloZAVqMTGiZADr8aCNkoTaj4HQDEbQ4jVGqX1YkgHkrZAbKwdJ7t72ApxdXkwncsMZCFgAS9otkPKdYUvyQv8kb84J3O329l6kkvRjzbCWlR5KtEwslgZDZD';
+const ACCESS_TOKEN = 'EAAm2xmInmFsBRIPFi73MrInfVdyG3GCDeffc0SZChxpF9tUgqijMHRBWGYgfgieO3zqZCYINc4QHaov2S6PFycsl7W4BKXuzmiOlnBZACD5bMCU9UG7mUp0fBH3PcBZCcM3lY9Gx6XyvdBwZCyixjT8U26sN45g16jiZBjKM6W9R3hXUIaSAFNobnIkIk6kWaFNjxbBcFEerEmC0C6c36ZCA8VdY5WQ3N7ZC74vLIMST1SqIP8wd4RhmZBZB8tE1vB3Rk3pbHq0HJjZAqtbgJ4fTbiCVZAO9I5ZAOLQZDZD';
 
 const server = http.createServer(async (req, res) => {
   if (req.method === 'POST') {
@@ -22,13 +21,11 @@ const server = http.createServer(async (req, res) => {
         const metaPayload = {
           data: [
             {
-              event_name: 'Lead', // Ou 'Purchase'
+              event_name: 'Purchase', // Ou 'Lead'
               event_time: Math.floor(Date.now() / 1000),
-              action_source: 'website',
               user_data: {
-                em: event.customer_email ? crypto.createHash('sha256').update(event.customer_email.toLowerCase()).digest('hex') : undefined,
+                em: event.customer_email ? event.customer_email.toLowerCase() : undefined,
               },
-              test_event_code: 'TEST59669',
             }
           ]
         };
@@ -45,7 +42,7 @@ const server = http.createServer(async (req, res) => {
       } catch (error) {
         console.error('Erreur:', error.message);
         res.writeHead(500);
-        res.end(JSON.stringify({ error: error.response ? error.response.data : error.message }));
+        res.end('Erreur');
       }
     });
   }
